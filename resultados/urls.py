a52fcc2316f40b13302d5710e62509fbd024cf5c
Rodrigo Sales <164502711+RodrigoBahia10@users.noreply.github.com>
2025-06-04 10:34:27 -0300

    Add user authentication with login page

--- resultados/urls.py
+++ resultados/urls.py
@@ -1,6 +1,7 @@
 # resultados/urls.py
 
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views # Importa as views do app atual
 
 # Define um nome para o conjunto de URLs deste app, para evitar conflitos
@@ -9,4 +10,6 @@ app_name = 'resultados'
 urlpatterns = [
     # Quando a URL raiz do app for acessada, chame a view 'pagina_inicial'
     path('', views.pagina_inicial, name='pagina_inicial'),
+    path('login/', auth_views.LoginView.as_view(template_name='resultados/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
